9f678ff933325e0db3f984f22273ff2574d2bb63
apereverzin <andrey.pereverzin@hotmail.com>
2017-09-25 00:02:21 +0100

    Added Rating structure and Rating related functions

--- src/main/solidity/WrisxToken.sol
+++ src/main/solidity/WrisxToken.sol
@@ -1,8 +1,31 @@
 pragma solidity ^0.4.10;
 
+contract owned {
+    address public owner;
 
-contract WrisxToken {
+    function owned() {
+        owner = msg.sender;
+    }
 
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
+
+contract WrisxToken is owned {
+
+    struct Rating {
+    uint totalRating;
+    uint number;
+    uint initialized;
+    }
+
+    mapping (address => Rating) public riskExpertRatings;
     mapping (address => uint256) public balanceOf;
 
     string public name;
@@ -22,4 +45,23 @@ contract WrisxToken {
         balanceOf[msg.sender] = _totalSupply;
     }
 
+    function getBalance(address member) constant returns(uint256 balance) {
+        return this.balanceOf(member);
+    }
+
+    function getExpertInitialized(address expert) constant returns(uint init) {
+        return riskExpertRatings[expert].initialized;
+    }
+
+    function getExpertTotalRating(address expert) constant returns(uint totalRating) {
+        return riskExpertRatings[expert].totalRating;
+    }
+
+    function registerRiskExpert() returns (bool success) {
+        require (riskExpertRatings[msg.sender].initialized == 0);
+
+        riskExpertRatings[msg.sender].initialized = 1;
+
+        return true;
+    }
 }
